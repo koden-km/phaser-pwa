e57a5b47350349e411f1224cd684e1269b5d5f46
Kevin Millar <koden.km@gmail.com>
2019-07-21 18:24:00 +1000

    Service work is not caching files for GH pages.

--- sw.js
+++ sw.js
@@ -1,13 +1,23 @@
 var cacheName = 'phaser-v1';
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/img/logo.png',
-    '/img/icon-192.png',
-    '/img/icon-256.png',
-    '/img/icon-512.png',
-    '/js/game.js',
-    '/css/style.css',
+    // '/',
+    // '/index.html',
+    // '/img/logo.png',
+    // '/img/icon-192.png',
+    // '/img/icon-256.png',
+    // '/img/icon-512.png',
+    // '/js/game.js',
+    // '/css/style.css',
+
+    // '/',
+    'index.html',
+    'img/logo.png',
+    'img/icon-192.png',
+    'img/icon-256.png',
+    'img/icon-512.png',
+    'js/game.js',
+    'css/style.css',
+
     'https://cdn.jsdelivr.net/gh/photonstorm/phaser@3.18.1/dist/phaser.min.js'
 ];
 
